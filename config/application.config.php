33ff5c7e279fca84dab9cac4cf72542c70a5c976
Patrick Schlaepfer <patrick@schlaepfer.com>
2015-05-09 00:10:19 +0200

    Added Camunda PHP SDK

--- config/application.config.php
+++ config/application.config.php
@@ -4,6 +4,7 @@ return array(
     'modules' => array(
         'Application',
 		'Album',
+		'Camunda',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
---
+++ module/Camunda/Module.php
@@ -0,0 +1,18 @@
+<?php
+namespace Camunda;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+ 
+ class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+ {
+     public function getAutoloaderConfig() {}
+
+     public function getConfig()
+     {
+         return include __DIR__ . '/config/module.config.php';
+     }
+	 
+ }
+ ?>
